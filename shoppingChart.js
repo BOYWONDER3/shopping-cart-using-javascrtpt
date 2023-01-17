af8e5eb2566c1df7fc742beb409bc31623f555d5
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-01-17 03:49:17 +0100

    sixth commit

--- shoppingChart.js
+++ shoppingChart.js
@@ -7,8 +7,14 @@ let shoppingCart = []
 const IMAGE_URL = 'https://dummyimage.com/210x130'
 const cartItemTemplate = document.querySelector("#cart-item-template")
 const cartItemContainer = document.querySelector('[data-cart-items]')
+const cartQuantity = document.querySelector('[data-cart-quantity]')
+const cartTotal = document.querySelector('[data-cart-total]')
+const cart = document.querySelector('[data-cart]')
 
-export function setupShoppingCart() {}
+
+export function setupShoppingCart() {
+    renderCart()
+}
 
 
 // Remove items from cart
@@ -29,11 +35,58 @@ cartButton.addEventListener('click', (e) =>{
   // calculate an accurate total
 
  export function addToCart (id) {
-    shoppingCart.push({ id: id, quantity: 1 })
+    const existingItem = shoppingCart.find(entry => entry.id === id)
+    if (existingItem) {
+        existingItem.quantity++
+    } else {
+        shoppingCart.push({ id: id, quantity: 1 })
+    }
     renderCart()
   }
 
-function renderCart () {
+
+  function removeFromCart(id) {
+    const existingItem = shoppingCart.find(entry => entry.id === id)
+    if (existingItem == null) return
+    shoppingCart = shoppingCart.filter(entry => entry.id !== id)
+    renderCart()
+  }
+
+
+  function renderCart() {
+    if(shoppingCart.length === 0) {
+        hideCart()
+    } else{
+        showCart()
+        renderCartItems()
+    }
+  }
+
+  function hideCart() {
+    cart.classList.add('invisible')
+    cartItemsWrapper.classList.add('invisible')
+  }
+
+  function showCart() {
+    cart.classList.remove('invisible')
+  }
+
+
+
+
+
+function renderCartItems () {
+
+    cartQuantity.innerText = shoppingCart.length
+
+    const totalCents = shoppingCart.reduce((sum, entry) => {
+        const item = items.find(i => entry.id === i.id)
+        return sum + item.priceCents * entry.quantity
+    }, 0)
+
+    cartTotal.innerText = formatCurrency(totalCents / 100)
+
+    cartItemContainer.innerHTML = ''
     shoppingCart.forEach(entry => {
         const item = items.find(i => entry.id === i.id)
         const cartItem = cartItemTemplate.content.cloneNode(true)
@@ -47,8 +100,11 @@ function renderCart () {
     const image = cartItem.querySelector("[data-image]")
     image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
 
-    const quantity = cartItem.querySelector("[data-quantity]")
+
+    if(entry.quantity > 1) {
+        const quantity = cartItem.querySelector("[data-quantity]")
     quantity.innerText = `x${entry.quantyity}`
+    }
 
     const price = cartItem.querySelector("[data-price]")
     price.innerText = formatCurrency(item.priceCents * entry.quantity / 100)
